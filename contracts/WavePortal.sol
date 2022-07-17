f9cf081e5705a814146eadb2c4c93367bc26d6c3
yuk1ch1 <sma2ww@gmail.com>
2022-07-17 20:50:53 +0900

    impl: call wave & getTotalWaves

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -16,7 +16,7 @@ contract WavePortal {
     }
 
     function getTotalWaves() public view returns (uint256) {
-        console.log("We have %d total wavex!", totalWaves);
+        console.log("We have %d total waves!", totalWaves);
         return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -10,11 +10,34 @@ const runMain = async () => {
 
 // run.js
 const main = async () => {
+    // Hardhatが提供する関数。任意のアドレスを返す。
+    // コントラクト所有者のウォレットアドレスとwaveを送るユーザーのウォレットアドレスを変数ownerとrandomPersonに格納
+    // Signer object: トランザクションをcontractや他のイーサaccountへ送るために使われる
+    // getSignersでつながっているノード状のaccountのリストを取得
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     const wavePortal = await waveContract.deployed();
 
-    console.log("WavePortal address: ", wavePortal.address);
+    console.log("Contract deployed to:", wavePortal.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    // wave関数ではブロックチェーン上に書き込みが発生するのでガス代がかかるため、waveを送ろうしているユーザーからの承認を取る必要がある
+    // なので承認を待つ間コントラクトからの応答をフロント側は待つ必要がある
+    // 承認されるとawait waveTxn.wait()が実行されて、トランザクションの結果が取得される
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
+    waveTxn = await waveContract.connect(randomPerson).wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
 };
 
 runMain();
