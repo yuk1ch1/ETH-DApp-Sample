70075d97f9ba5e20c2d280f7280b7760fd77da5e
yuk1ch1 <sma2ww@gmail.com>
2022-07-17 14:54:57 +0900

    impl constructor to print log

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Here is my first smart contract!");
+    }
+}
