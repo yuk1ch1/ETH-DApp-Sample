aeafd0b810a1768dddcf9b959c6d5578fce52f21
yuk1ch1 <sma2ww@gmail.com>
2022-07-17 20:01:46 +0900

    impl: function to change totalWaves & to get storaged waves on chain

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,7 +4,19 @@ pragma solidity ^0.8.15;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("Here is my first smart contract!");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s haas waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total wavex!", totalWaves);
+        return totalWaves;
     }
 }
