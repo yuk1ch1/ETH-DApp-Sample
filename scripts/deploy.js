30064b8593e62db19520103405d72fb4a8578a49
yuk1ch1 <sma2ww@gmail.com>
2022-07-17 21:33:36 +0900

    impl: script to deploy contract on local chain

---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+// 本番用ファイル
+const main = async () => {
+    const [deployer] = await hre.ethers.getSigners();
+    const accountBalance = await deployer.getBalance();
+    const waveContract = await hre.ethers.getContractFactory("WavePortal");
+    const wavePortal = await waveContract.deploy();
+
+    console.log("Deploying contracts with account: ", deployer.address);
+    console.log("Account balance; ", accountBalance.toString());
+    console.log("Contract deployed to: ", wavePortal.address);
+    console.log("Contract deployed by: ", deployer.address);
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+runMain();
