f3da7621f84bf112e360c94c40c10544032df86d
yuk1ch1 <sma2ww@gmail.com>
2022-08-06 15:13:48 +0900

    101-ETH-dApp: section4完了

--- my-wave-portal/contracts/WavePortal.sol
+++ my-wave-portal/contracts/WavePortal.sol
@@ -7,6 +7,7 @@ import "hardhat/console.sol";
 
 contract WavePortal {
     uint256 totalWaves; // コントラクト変数を定義し、初期値を設定しない場合には各型のデフォルト値が可能されるみたい(https://book.ethereum-jp.net/solidity/var_and_data_type)
+    uint256 private seed; // 乱数生成のための基盤となるシード(種)を作成
 
     // NewWaveイベントの作成
     event NewWave(address indexed from, uint256 timestamp, string message);
@@ -28,36 +29,61 @@ contract WavePortal {
 
     Wave[] waves;
 
+    // address => uint mappingはアドレスと数値を紐づける
+    mapping(address => uint256) public lastWaveAt;
+
     // payableを付けることで、コントラクトに送金機能を実装している
     constructor() payable {
         console.log("We have been constructed!");
+
+        // 初期シードを設定
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory _message) public {
-        // emit NewWave(msg.sender, block.timestamp, _message);
+        // 現在の時刻が、前回waveを送信した時刻より15分以上過ぎていることを確認
+        require(lastWaveAt[msg.sender] + 15 minutes < block.timestamp, "Wait 15m");
+        
+        // ユーザーの現在のタイムスタンプを更新する
+        lastWaveAt[msg.sender] = block.timestamp;
 
         totalWaves += 1;
-        console.log("%s waved w/ message %s", msg.sender, _message);
+        console.log("%s has waved", msg.sender);
 
         // 「👋（wave）」とメッセージを配列に格納。
         waves.push(Wave(msg.sender, _message, block.timestamp));
 
+        // ユーザーのために乱数を生成
+        // block.difficulty: ブロック承認(= マイニング)の何度をマイナーに通知するための値。ブロック内のトランザクションが多いほど、難易度は高くなる
+        // block.timestamp: ブロックが処理されている時のUNIXタイムスタンプ
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+
+        console.log("Random # generatd: %d", seed);
+
+        // ユーザーがETHを獲得する確率を50%に設定
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            // コントラクトへ関与してくれた、「👋（wave）」を送ってくれたユーザーに0.0001ETHを送る
+            uint256 prizeAmount = 0.0001 ether;
+            // address(this).balanceでコントラクが持つ残高を確認
+            // 賞金が残高より少ないか確認している。falseならトランザクションが中止され、右側の文言が表示される。trueなら引き続き後続の処理が実行される
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdra more money than the contract has"
+            );
+
+            // コントラクトに関与してくれたユーザーにethを送金
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+
+            // 送金結果が失敗ならトランザクションが中止され、右側の文言が表示される？
+            require(success, "Failed to withdraw mone from contract.");
+        } else {
+            console.log("%s did not win.", msg.sender);
+        }
+
         // フロントエンドへイベント送信
         emit NewWave(msg.sender, block.timestamp, _message);
-
-        // コントラクトへ関与してくれた、「👋（wave）」を送ってくれたユーザーに0.0001ETHを送る
-        uint256 prizeAmount = 0.0001 ether;
-        // address(this).balanceでコントラクが持つ残高を確認
-        // 賞金が残高より少ないか確認している。falseならトランザクションが中止され、右側の文言が表示される？
-        require(
-            prizeAmount <= address(this).balance,
-            "Trying to withdra more money than the contract has"
-        );
-
-        // コントラクトに関与してくれたユーザーにethを送金
-        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-        // 送金結果が失敗ならトランザクションが中止され、右側の文言が表示される？
-        require(success, "Failed to withdraw mone from contract.");
     }
 
     function getAllWaves() public view returns (Wave[] memory) {
--- my-wave-portal/scripts/run.js
+++ my-wave-portal/scripts/run.js
@@ -1,4 +1,4 @@
-// run.js
+// run.js(ローカル実行用)
 // ethersは元々hardhat側でグローバルアクセス可能なものとして提供されている。なのでわざわざフィールド変数として定義すると冗長になるため不要。
 const { ethers } = require("hardhat");
 
@@ -15,10 +15,9 @@ const runMain = async () => {
 // function Dummy(引数名) {}, funcion(引数名) {}, 引数名 => {} の3つがjsの関数の書き方
 // 上記３つのうち最後のアロー式を用いた関数の書き方
 const main = async () => {
-    // 任意のアドレスを返す
     const [owner, randomPerson] = await ethers.getSigners();
 
-    // コントラクトをデプロイするためには追加情報が必要。ContractFactoryはコントラクトのデプロイをサポートする。そのためにhre.ethers.getContractFactoryでContractFactoryのインスタンスを生成
+    // コントラクトをデプロイするためには追加情報が必要。そのためにhre.ethers.getContractFactoryで生成されるContractFactoryはコントラクトのデプロイをサポートする
     const waveContractFactory = await ethers.getContractFactory("WavePortal");
 
     // このコントラクトのためだけにローカルにEthereumネットワークを作成し、コントラクトのデプロイを開始
@@ -31,26 +30,25 @@ const main = async () => {
     const wavePortal = await waveContract.deployed();
 
     console.log("Contract deployed to:", wavePortal.address);
-    console.log("Contract deployed by:", owner.address);
 
     // コントラクトの残高を取得し、結果を出力
     let contractBalance = await ethers.provider.getBalance(wavePortal.address);
     // wei単位の残高をETH単位に変換した上で出力
     console.log("Contract balance;", ethers.utils.formatEther(contractBalance));
 
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
-    console.log(waveCount.toNumber());
 
-    // waveを送る
-    let waveTxn = await waveContract.wave("A message!");
+    // waveを2回送るシュミレーション
+    const waveTxn = await waveContract.wave("This is wave #1");
     await waveTxn.wait(); // トランザクションが承認されるのを待つ(テスト: 1回目)
+
+    const waveTxn2 = await waveContract.wave("This is wave #2");
+    await waveTxn2.wait();
 
     // waveした後のコントラクトの残高を取得し、結果を出力
     contractBalance = await ethers.provider.getBalance(waveContract.address);
     console.log("Contract balance", ethers.utils.formatEther(contractBalance));
 
-    let allWaves = await waveContract.getAllWaves()
+    let allWaves = await waveContract.getAllWaves();
     console.log(allWaves);
 };
 
